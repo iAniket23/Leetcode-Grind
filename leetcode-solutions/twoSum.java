b9691761a06886c6cec8ac7898ca5edc14b96d72
Aniket Mishra <ianiket23@gmail.com>
2021-06-03 17:54:48 -0600

    Update twoSum.java

--- leetcode-solutions/twoSum.java
+++ leetcode-solutions/twoSum.java
@@ -26,7 +26,7 @@ Only one valid answer exists.
  
 */
 
-// Solution 1
+// My Solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
@@ -48,7 +48,7 @@ class Solution {
 }
 
 
-// Solution 2 - It includes throw exception. Better looping and return mechanics
+// Solution 1 - It includes throw exception. Better looping and return mechanics
 public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
         for (int j = i + 1; j < nums.length; j++) {
@@ -60,7 +60,7 @@ public int[] twoSum(int[] nums, int target) {
     throw new IllegalArgumentException("No two sum solution");
 }
 
-// Soultion 3 - Hash Map
+// Soultion 2 - Hash Map
 public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
