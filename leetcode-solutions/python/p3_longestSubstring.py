288ec3aa9eb64cf929df8bcbd90ca90b32acbce0
Aniket Mishra <ianiket23@gmail.com>
2022-01-24 11:02:29 -0700

    Create p3_longestSubstring.py

---
+++ leetcode-solutions/python/p3_longestSubstring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = [0] * 128
+
+        left = right = 0
+
+        res = 0
+        while right < len(s):
+            r = s[right]
+            print(r)
+            print(ord(r))
+            chars[ord(r)] += 1
+
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+
+            right += 1
+        
+        return res
+    
+s = Solution()
+print(s.lengthOfLongestSubstring("pwwkew"))
